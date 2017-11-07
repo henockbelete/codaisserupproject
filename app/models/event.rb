19cb09c453c8e56d4c6cccd3f2a8e2c9de44e9a6
henockbelete <etyeheny@gmail.com>
2017-11-07 14:06:37 +0100

    one to many: controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,15 @@ class Event < ApplicationRecord
 
 
    validates :name, presence: true
+   #validates :starts_at: presence: 2017-11-06
+   #validates :ends_at: presence: 2017-16-06
    validates :description, presence: true, length: { maximum: 500 }
+
+
+   # def init
+    #     self.number  ||= 0.0           #will set the default value only if it's nil
+    #     self.address ||= build_address #let's you set a default association
+    #   end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
 
 
+   resources :users, only: [:show]
+   resources :events, except: [:destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
