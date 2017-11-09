e5e69ed59d8fd153b77ba63906fe5da4d061016c
henockbelete <etyeheny@gmail.com>
2017-11-09 16:11:06 +0100

    Create Photo: Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,8 @@ class EventsController < ApplicationController
 
    def show;
    @event = Event.find(params[:id])
+   @photos = @event.photos
+
    end
 
    def new
@@ -19,23 +21,43 @@ class EventsController < ApplicationController
      @event = current_user.events.build(event_params)
 
      if @event.save
-       redirect_to @event, notice: "Event created successfully"
+       image_params.each do |image|
+       @event.photos.create(image: image)
+     end
+
+     redirect_to @event, notice: "Event created successfully"
      else
        render :new
      end
+
    end
 
-   def edit; end
+   def edit;
+
+     if current_user.id == @event.user.id
+         @photos = @event.photos
+       else
+         redirect_to root_path, notice: "You don't have permission."
+       end
+   end
 
    def update
      if @event.update(event_params)
-       redirect_to @event, notice: "Event updated"
+       image_params.each do |image|
+       @event.photos.create(image: image)
+     end
+     redirect_to edit_event_path(@event), notice: "Event successfully updated"
      else
        render :edit
      end
+
   end
 
+
+
 private
+
+
 
   def set_event
     @event = Event.find(params[:id])
@@ -50,4 +72,10 @@ private
       )
 
   end
+
+  def image_params
+    params[:images].present? ? params.require(:images) : []
+  end
+
+  
 end
