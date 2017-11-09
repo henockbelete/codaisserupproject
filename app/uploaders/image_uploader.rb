710ccdb48855d10c38c916031258b2721ceb726b
henockbelete <etyeheny@gmail.com>
2017-11-09 16:28:30 +0100

    Create Photo: Views

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,14 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  version :thumbnail do
+    eager
+    resize_to_fit(300, 300)
+    cloudinary_transformation :quality => 80
+  end
 end
+
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
